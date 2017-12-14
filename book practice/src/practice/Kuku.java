6ef63c93969e65b6aced7b08efca0b6fb31439d8
aaa <aa@aa.com>
2017-12-14 17:06:51 +0900

    backup

---
+++ book practice/src/practice/Kuku.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Kuku {
+
+	public static void main(String[] args) {
+	for (int i = 1; i<10; i++){
+		for(int a =1; a<10; a++){
+			System.out.print(i*a);
+			System.out.print(" ");
+		}
+		System.out.println("");
+	}
+
+
+	}
+}
