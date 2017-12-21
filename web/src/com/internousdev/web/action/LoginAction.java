e863c1222e6f09182175c5166712b9f29cde6b79
1sd <dai1991diver@gmail.com>
2017-12-21 17:48:50 +0900

    12/21

--- web/src/com/internousdev/web/action/LoginAction.java
+++ web/src/com/internousdev/web/action/LoginAction.java
@@ -11,48 +11,16 @@ import com.internousdev.web.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport implements SessionAware {
-
+	
 	private String username;
 	private String password;
-	private List<LoginDTO> loginDTOList = new ArrayList<LoginDTO>();
+	private List<LoginDTO> LoginDTOList=new ArrayList<LoginDTO();
 	private Map<String, Object> session;
-
+	
 	public String execute(){
 		String ret = ERROR;
-		LoginDAO dao = new LoginDAO();
+		LoginDAO  dao = new LoginDAO();
+		
+	}
 
-		loginDTOList = dao.select(username, password);
-		if (this.username.equals(loginDTOList.get(0).getUsername())
-		&& this.password.equals(loginDTOList.get(0).getPassword())){
-			session.put("loginDTOList",loginDTOList);
-			ret = SUCCESS;
-		}else{
-			ret =ERROR;
-		}
-		return ret;
-	}
-	public String getUsername(){
-		return username;
-	}
-	public void setUsername(String username){
-		this.username = username;
-	}
-	public String getPassword(){
-		return password;
-	}
-	public void setPassword(String password){
-		this.password = password;
-	}
-	public List<LoginDTO> getLoginDTOList(){
-		return loginDTOList;
-	}
-	public void setLoginDTOList(List<LoginDTO> loginDTOList){
-		this.loginDTOList = loginDTOList;
-	}
-	public Map<String, Object>getSession(){
-		return session;
-	}
-	public void setSession(Map<String, Object> session){
-		this.session = session;
-	}
 }
--- web2/src/com/internousdev/web2/action/package-info.java
+++
@@ -1,2 +0,0 @@
-
-package com.internousdev.web2.action;
